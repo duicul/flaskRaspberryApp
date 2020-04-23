4619031fb59ab925247fcb667bdb5368ae280273
duicul <duicul@yahoo.com>
2020-04-23 23:23:00 +0300

    improve status script

--- flaskstatus.sh
+++ flaskstatus.sh
@@ -1,1 +1,9 @@
-ps aux | grep -E '((^USER)|(flask))'
+#!/bin/bash
+process="flask"
+if [ $# -gt 0 ]
+then
+  process=$1
+fi
+echo $process
+ps aux | head -n 1
+ps aux | grep -E "((^USER)|($process))"
