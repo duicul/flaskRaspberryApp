f82967633be2eb94258df7c5000a2ed4bb5a81c3
duicul <32304082+duicul@users.noreply.github.com>
2020-10-15 20:58:09 +0300

    round predicted decimal places

--- flask_pydev/Login.py
+++ flask_pydev/Login.py
@@ -208,10 +208,10 @@ def  home_station_temperature_data():
         		data_t2=[t[i]["temp2"] for i in range(len(t)-dataset_size,len(t))]
         		
         		poly_fit_t1 = np.poly1d(np.polyfit(np.array(range(dataset_size)),np.array(data_t1),pol_grade))
-        		pol_regr_y_t1=[poly_fit_t1(xi) for xi in range(dataset_size,dataset_size+predict_len)]
+        		pol_regr_y_t1=[round(poly_fit_t1(xi),2) for xi in range(dataset_size,dataset_size+predict_len)]
         		
         		poly_fit_t2 = np.poly1d(np.polyfit(np.array(range(dataset_size)),np.array(data_t2),pol_grade))
-        		pol_regr_y_t2=[poly_fit_t2(xi) for xi in range(dataset_size,dataset_size+predict_len)]  
+        		pol_regr_y_t2=[round(poly_fit_t1(xi),2) for xi in range(dataset_size,dataset_size+predict_len)]  
         except Exception:
         	logging.getLogger('werkzeug').error(str(traceback.format_exc()))
         	pol_regr_y_t1=[]
