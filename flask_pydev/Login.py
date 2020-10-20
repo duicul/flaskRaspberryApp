c1199839da778daa5a40393fb97700a7bcc37d8b
duicul <32304082+duicul@users.noreply.github.com>
2020-10-20 13:46:08 +0300

    Match indexes for filtered temps

--- flask_pydev/Login.py
+++ flask_pydev/Login.py
@@ -204,13 +204,13 @@ def  home_station_temperature_data():
         pol_regr_y_t2=[]
         try:
         	if dataset_size > 0:
-        		data_t1=list(filter(lambda x:x>-127,[t[i]["temp1"] for i in range(len(t)-dataset_size,len(t))]))
-        		data_t2=list(filter(lambda x:x>-127,[t[i]["temp2"] for i in range(len(t)-dataset_size,len(t))]))
+        		data_t1=list(filter(lambda x:x>-127,[(i-dataset_size,t[i]["temp1"]) for i in range(len(t)-dataset_size,len(t))]))
+        		data_t2=list(filter(lambda x:x>-127,[(i-dataset_size,t[i]["temp2"]) for i in range(len(t)-dataset_size,len(t))]))
         		
-        		poly_fit_t1 = np.poly1d(np.polyfit(np.array(range(dataset_size)),np.array(data_t1),pol_grade))
+        		poly_fit_t1 = np.poly1d(np.polyfit(np.array([x[0] for x in data_t1]),np.array([x[1] for x in data_t1]),pol_grade))
         		pol_regr_y_t1=[round(poly_fit_t1(xi),2) for xi in range(dataset_size,dataset_size+predict_len)]
         		
-        		poly_fit_t2 = np.poly1d(np.polyfit(np.array(range(dataset_size)),np.array(data_t2),pol_grade))
+        		poly_fit_t2 = np.poly1d(np.polyfit(np.array([x[0] for x in data_t2]),np.array([x[1] for x in data_t2]),pol_grade))
         		pol_regr_y_t2=[round(poly_fit_t2(xi),2) for xi in range(dataset_size,dataset_size+predict_len)]  
         except Exception:
         	logging.getLogger('werkzeug').error(str(traceback.format_exc()))
