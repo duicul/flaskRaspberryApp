dd237a83663df5f8ff9082050417cbacc26876a5
duicul <32304082+duicul@users.noreply.github.com>
2020-11-08 19:42:18 +0200

    modify uwsgi config

--- flask_pydev/Createuwsgiconfig.py
+++ flask_pydev/Createuwsgiconfig.py
@@ -12,7 +12,7 @@ str_out+=BASE_DIR
 str_out+="\n"
 str_out+="logto = "+BASE_DIR+"/logs/flaskRaspPi-uwsgi.log\n"
 str_out+="plugins = python3\n"
-str_out+="module=wsgi:app\n"
+str_out+="module=flask_pydev.wsgi:app\n"
 str_out+="wsgi-file = "+BASE_DIR+"/wsgi.py # customize with the relative path to your wsgi.py file \n"
 
 str_out+="workers = 1"
