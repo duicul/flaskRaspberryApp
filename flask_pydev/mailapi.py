9781831687d7a23254bb3290ec08958edc1542c9
duicul <32304082+duicul@users.noreply.github.com>
2021-01-05 12:35:37 +0200

    catch mail login error

--- flask_pydev/mailapi.py
+++ flask_pydev/mailapi.py
@@ -3,7 +3,7 @@ import json
 from email.mime.text import MIMEText
 import logging
 import traceback
-
+from smtplib import SMTPAuthenticationError
 def read_mail_config():
     try:
         file=open("mailconfig.json","r")
@@ -18,23 +18,29 @@ def read_mail_config():
     return file_json
 
 def send_mail(msg):
+    try:
+        config_data=read_mail_config()
     
-    config_data=read_mail_config()
-    
-    msg = MIMEText(msg)
+        msg = MIMEText(msg)
 
-    msg['Subject'] = 'Critical temperature'
-    msg['From'] = 'home_station'
-    msg['To'] = 'client'
+        msg['Subject'] = 'Critical temperature'
+        msg['From'] = 'home_station'
+        msg['To'] = 'client'
     
-    receivers = config_data["receivers"]
-    main_acc= config_data["mail_account"]
-    sender = main_acc["mail"]
-    with smtplib.SMTP('smtp.gmail.com', 587) as server:
-            server.starttls()
-            server.login(main_acc["user"], main_acc["pass"])
-            server.sendmail(sender, receivers, msg.as_string())
-            #print("Successfully sent email")
-
+        receivers = config_data["receivers"]
+        main_acc= config_data["mail_account"]
+        sender = main_acc["mail"]
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            try:
+                server.starttls()
+                server.login(main_acc["user"], main_acc["pass"])
+                server.sendmail(sender, receivers, msg.as_string())
+                #print("Successfully sent email")
+            except SMTPAuthenticationError as e:
+                logging.getLogger("monitor_logger").error(str(traceback.format_exc()))
+                pass
+    except Exception as e:
+        logging.getLogger("monitor_logger").error(str(traceback.format_exc()))
+        pass
 if __name__ == "__main__":
     send_mail("Temperatura atinsa : 95C ")
