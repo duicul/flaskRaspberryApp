033992748dfa962ab3811e642523840d022cd355
duicul <32304082+duicul@users.noreply.github.com>
2021-06-24 08:42:55 +0300

    Add login redirect for home station urls

--- flask_pydev/Login.py
+++ flask_pydev/Login.py
@@ -1,4 +1,4 @@
-from flask import Flask,session,request,render_template,redirect, url_for
+from flask import Flask,session,request,render_template,redirect, url_for,Response
 #from inputpin import InputPin
 #from time import sleep
 import json
@@ -30,16 +30,16 @@ app.logger.setLevel(logging.WARNING)
 app.logger.addHandler(handler)
 
 try:
-	file=open("data.json","r")
-	file_json=json.load(file)
-	home_station_url=file_json["url"]
-	polling_period=file_json["period"]
-	file.close()
+    file=open("data.json","r")
+    file_json=json.load(file)
+    home_station_url=file_json["url"]
+    polling_period=file_json["period"]
+    file.close()
 except:
-	file_json={"url":home_station_url,"period":polling_period}
-	file=open("data.json","w")
-	json.dump(file_json,file)
-	file.close()
+    file_json={"url":home_station_url,"period":polling_period}
+    file=open("data.json","w")
+    json.dump(file_json,file)
+    file.close()
 
 
 #td=Temperature_Data("measure.db",home_station_url,'werkzeug')
@@ -48,6 +48,11 @@ vd=Voltage_Data("measure.db",home_station_url,'werkzeug')
 acd=AC_Data("measure.db",home_station_url,'werkzeug')
 od=Outside_Data("measure.db",home_station_url,'werkzeug')
 aut=Authorization()
+
+
+@app.errorhandler(401)
+def custom_401(error):
+    return Response('<Why access is denied string goes here...>', 401, {'WWW-Authenticate':'Basic realm="Login Required"'})
 
 @app.before_request
 def make_session_permanent():
@@ -83,30 +88,51 @@ def login():
     
 @app.route('/current_timestamp')
 def current_timestamp():
-	return str(tsd.current_timestamp())
-	
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    return str(tsd.current_timestamp())
+
+
 @app.route('/cpu_gpu_temp')
 def cpu_gpu_temp():
-	temps = os.popen('vcgencmd measure_temp').read().replace("\n","<br>")
-	return temps
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    temps = os.popen('vcgencmd measure_temp').read().replace("\n","<br>")
+    return temps
 
 @app.route('/memory_usage')
 def memory_usage():
-	memory = os.popen('free -ht').read().replace("\n","<br>").replace(" ","&nbsp;&nbsp;")
-	return memory
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    memory = os.popen('free -ht').read().replace("\n","<br>").replace(" ","&nbsp;&nbsp;")
+    return memory
 
 @app.route('/disk_usage')
 def disk_usage():
-	memory = os.popen('df -H').read().replace("\n","<br>").replace(" ","&nbsp;&nbsp;")
-	return memory
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    memory = os.popen('df -H').read().replace("\n","<br>").replace(" ","&nbsp;&nbsp;")
+    return memory
 
 @app.route('/data_retr')
 def data_status():
-	return "okay stubbed" #data_retr.showdata()
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    return "okay stubbed" #data_retr.showdata()
 
 @app.route('/success/<name>')
 def success(name):
-   return 'welcome %s' % name
+    return 'welcome %s' % name
 
 @app.route('/covid_data_all/<case_type>/<api>/<data_type>/<predict_len>/<pol_grade>',methods = ['POST'])
 def extract_data_pol(api,predict_len,pol_grade,case_type,data_type):
@@ -134,156 +160,166 @@ def extract_regions(api,case_type,data_type):
 
 @app.route('/force_poll')
 def force_poll():
-	tsd.poll_value()
-	vd.poll_value()
-	acd.poll_value()
-	od.poll_value()
-	return ""
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    tsd.poll_value()
+    vd.poll_value()
+    acd.poll_value()
+    od.poll_value()
+    return ""
 
 @app.route('/convert_old')
 def convert_old():
-	tsd.convert_old()
-	return "success"
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    tsd.convert_old()
+    return "success"
+    
 @app.route('/temperature')
 def temperature():
-        data=tsd.extract_last()
-        if data==None:
-                return {}
-        #print(data)
-        #r = requests.get(home_station_url+"/temperature")
-        return str(data)#{"date":data[1],"temp1":data[2],"temp2":data[3]}
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    data=tsd.extract_last()
+    if data==None:
+        return {}
+    return str(data)#{"date":data[1],"temp1":data[2],"temp2":data[3]}
         
 
 @app.route('/voltage')
 def voltage():
-        data=vd.extract_last()
-        if data==None:
-                return {}
-        #print(data)
-        #r = requests.get(home_station_url+"/voltage")
-        return json.dumps({"date":data[1],"volt1":data[2]})
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    data=vd.extract_last()
+    if data==None:
+        return {}
+    return json.dumps({"date":data[1],"volt1":data[2]})
 
 def reset_config_weather():
-	file_json={"api_key":"random","city":"random"}
-	file=open("config_weather.json","w")
-	json.dump(file_json,file)
-	file.close()
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    file_json={"api_key":"random","city":"random"}
+    file=open("config_weather.json","w")
+    json.dump(file_json,file)
+    file.close()
 
 @app.route('/weather')
 def weather():
-	return json.dumps(od.poll_value())
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    return json.dumps(od.poll_value())
 
 @app.route('/ac')
 def ac():
-        data=acd.extract_last()
-        if data==None:
-                return {}
-        #print(data)
-        #r = requests.get(home_station_url+"/ac")
-        #print("data "+str(data))
-        return json.dumps({"date":data[1],"voltage":data[2],"current":data[3],"power":data[4],"energy":data[5]})
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    data=acd.extract_last()
+    if data==None:
+        return {}
+    return json.dumps({"date":data[1],"voltage":data[2],"current":data[3],"power":data[4],"energy":data[5]})
 
 @app.route('/home_station/voltage_data')
 def home_station_voltage_data():
-        #items=int()
-        volt=[]
-        interval=False
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    volt=[]
+    interval=False
+    try:
+        interval=True if request.args["interval"] == "true" else False
+    except:
+        pass
+    if(interval):
         try:
-        	interval=True if request.args["interval"] == "true" else False
+            volt = tsd.extract_all_between(request.args["fdate"], request.args["ldate"])
         except:
-        	pass
-        if(interval):
-        	try:
-        		volt = tsd.extract_all_between(request.args["fdate"], request.args["ldate"])
-        	except:
-        		logging.error(str(traceback.format_exc()))
-        else:
-        	try:
-        		volt = vd.extract_all_interval(request.args["items"])
-        	except:
-        		logging.error(str(traceback.format_exc()))
+            logging.error(str(traceback.format_exc()))
+    else:
+        try:
+            volt = vd.extract_all_interval(request.args["items"])
+        except:
+            logging.error(str(traceback.format_exc()))
         #print(data)
-        t=[]
-        for i in volt:
-            t.append({"date":i[1],"volt1":i[2]})    
-        return json.dumps(t)
+    t=[]
+    for i in volt:
+        t.append({"date":i[1],"volt1":i[2]})    
+    return json.dumps(t)
 
 @app.route('/home_station/ac_data')
 def home_station_ac_data():
-        #items=int(request.args["items"])
-        #print(items)
-        data=[]
-        interval=False
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    data=[]
+    interval=False
+    try:
+        interval=True if request.args["interval"] == "true" else False
+    except:
+        pass
+    if(interval):
         try:
-        	interval=True if request.args["interval"] == "true" else False
+            data = acd.extract_all_between(request.args["fdate"], request.args["ldate"])
         except:
-        	pass
-        if(interval):
-        	try:
-        		data = acd.extract_all_between(request.args["fdate"], request.args["ldate"])
-        	except:
-        		logging.error(str(traceback.format_exc()))
-        else:
-        	try:
-        		data = acd.extract_all_interval(request.args["items"])
-        	except:
-        		logging.error(str(traceback.format_exc()))
-        #print(data)
-        t=[]
-        for i in data:
-            t.append({"date":i[1],"voltage":i[2],"current":i[3],"power":i[4],"energy":i[5]})    
-        return json.dumps(t)
+            logging.error(str(traceback.format_exc()))
+    else:
+        try:
+            data = acd.extract_all_interval(request.args["items"])
+        except:
+            logging.error(str(traceback.format_exc()))
+    t=[]
+    for i in data:
+        t.append({"date":i[1],"voltage":i[2],"current":i[3],"power":i[4],"energy":i[5]})    
+    return json.dumps(t)
        
 @app.route('/home_station/temperature_data')
 def  home_station_temperature_data():
-        #items=int(request.args["items"])
-        #print(items)
-        temp=[]
-        interval=False
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    temp=[]
+    interval=False
+    try:
+        interval=True if request.args["interval"] == "true" else False
+    except:
+        pass
+    if(interval):
         try:
-        	interval=True if request.args["interval"] == "true" else False
+            temp = tsd.extract_all_between(request.args["fdate"], request.args["ldate"])
         except:
-        	pass
-        if(interval):
-        	try:
-        		temp = tsd.extract_all_between(request.args["fdate"], request.args["ldate"])
-        	except:
-        		logging.error(str(traceback.format_exc()))
-        else:
-        	try:
-        		temp = tsd.extract_all_interval(request.args["items"])
-        	except:
-        		logging.getLogger('werkzeug').error(str(traceback.format_exc()))
-        #print(data)      
-        t={}
-        """for i in temp:
-            t.append({"date":i[1],"temp_id":i[2],"temp":i[3]}) 
-        temp1=[{"date":i[1],"value":i[3]} for i in list(filter(lambda i :i[2]==1,temp))]
-        temp2=[{"date":i[1],"value":i[3]} for i in list(filter(lambda i :i[2]==2,temp))]
-        """
-        for id in range(1,11):
-        	t[str(id)]=[{"date":i[1],"value":i[3]} for i in list(filter(lambda i :i[2]==id,temp))]
-        print(t)
-        pol_grade=2
-        predict_len=8
-        dataset_size=20 if len(t)>10 else len(t)
-        pol_regr_y_t1=[]
-        pol_regr_y_t2=[]
-        result={"recorded":t,"predict":[]}
+            logging.error(str(traceback.format_exc()))
+    else:
+        try:
+            temp = tsd.extract_all_interval(request.args["items"])
+        except:
+            logging.getLogger('werkzeug').error(str(traceback.format_exc()))
+    t={}
+    for id in range(1,11):
+        t[str(id)]=[{"date":i[1],"value":i[3]} for i in list(filter(lambda i :i[2]==id,temp))]
+    print(t)
+    pol_grade=2
+    predict_len=8
+    dataset_size=20 if len(t)>10 else len(t)
+    pol_regr_y_t1=[]
+    pol_regr_y_t2=[]
+    result={"recorded":t,"predict":[]}
         
-        return json.dumps(result)
-
-@app.route('/home_station/remove_wrong_value')
-def remove_wrong():
-        tsd.remove_wrong_value()
-        vd.remove_wrong_value()
-        acd.remove_wrong_value()
-        return ""
-
-#@app.route('/home_station/clean')
-#def home_station_clean():
-#        clean_table();
-#        return ""
+    return json.dumps(result)
         
 @app.route('/home_station')
 def home_station():
@@ -308,25 +344,37 @@ def home_station():
 
 @app.route('/home_station/restart')
 def home_station_restart():
-	try:
-		requests.get(home_station_url+"/restart")
-	except:
-		pass
-	return "restarted"
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    try:
+        requests.get(home_station_url+"/restart")
+    except:
+        pass
+    return "restarted"
 
-@app.route('/remove_wrong_value')
+@app.route('/home_station/remove_wrong_value')
 def remove_wrong_value():
-	tsd.remove_wrong_value()
-	acd.remove_wrong_value()
-	vd.remove_wrong_value()
-	return ""
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    tsd.remove_wrong_value()
+    acd.remove_wrong_value()
+    vd.remove_wrong_value()
+    return ""
 
 @app.route('/covid')
 def index():
-	if 'username' in session:
-		username = session['username']
-	else:   username="anonymous"
-	return render_template('covid.html',name=username)
+    try:
+        user = session["user_name"]
+    except:
+        return redirect(url_for('home_station'))
+    if 'username' in session:
+        username = session['username']
+    else:   username="anonymous"
+    return render_template('covid.html',name=username)
 
 if __name__ == '__main__':
-   app.run(debug = True,host='0.0.0.0')     
+    app.run(debug = True,host='0.0.0.0')     
