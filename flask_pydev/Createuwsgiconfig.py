1efc9ac723ba73f6fd662666b326a02ebd09cf9e
duicul <32304082+duicul@users.noreply.github.com>
2024-08-15 09:22:56 +0300

    add venv

--- flask_pydev/Createuwsgiconfig.py
+++ flask_pydev/Createuwsgiconfig.py
@@ -18,7 +18,8 @@ str_out+="wsgi-file = "+BASE_DIR+"/wsgi.py # customize with the relative path to
 str_out+="workers = 1\n"
 str_out+="master = true\n"
 str_out+="processes = 4\n"
-str_out+="virtualenv = "+BASE_DIR+"/../../flask_app_venv\n"
+str_out+="processes = 4\n"
+str_out+="virtualenv = "+BASE_DIR+'/../../flask_app_venv/venv'
 f = open("/etc/uwsgi/apps-enabled/flaskRaspPi.ini", "w")
 f.write(str_out)
 f.close()
